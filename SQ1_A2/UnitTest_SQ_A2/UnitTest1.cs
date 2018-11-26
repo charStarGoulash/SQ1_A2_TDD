496c71e11f864e367784161ad4aaf2e33a7956dd
Attila Katona <Akatona8602@conestogac.on.ca>
2018-11-26 15:40:08 -0500

    Added a Test_FindHypotenuse TestMethod -Test FAILED

--- SQ1_A2/UnitTest_SQ_A2/UnitTest1.cs
+++ SQ1_A2/UnitTest_SQ_A2/UnitTest1.cs
@@ -22,5 +22,13 @@ namespace UnitTest_SQ_A2
             double result = obj.GetMissingAngle(200, 45);
             Assert.AreEqual(-1, result);
         }
+
+        [TestMethod]
+        public void Test_FindHypotenuse()
+        {
+            Triangle obj = new Triangle();
+            double result = obj.FindHypotenuse(25, 25);
+            Assert.AreEqual(35.36, result);
+        }
     }
 }
