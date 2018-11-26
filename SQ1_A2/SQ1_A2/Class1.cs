bd5f6ed1c2612ad9e902fa02a95014bbb5da37e6
Attila Katona <Akatona8602@conestogac.on.ca>
2018-11-26 16:24:13 -0500

    Added code to ensure the Test_FindHypotenuse_Exception passes, checking for negative numbers -test PASSED

--- SQ1_A2/SQ1_A2/Class1.cs
+++ SQ1_A2/SQ1_A2/Class1.cs
@@ -23,8 +23,15 @@ namespace SQ1_A2
 
         public double FindHypotenuse(double sideA, double sideB)
         {
-            double result = Math.Round( Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2)), 2);
-            return result;
+            if (sideA > 0 && sideB > 0)
+            {
+                double result = Math.Round(Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2)), 2);
+                return result;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         public double GetArea(double sideA, double sideB)
